73d968bc28d0f057ffe05b2fc09f0e87b87a3c54
laird-ikar <bguyot@student.42mulhouse.fr>
2023-12-14 18:16:17 +0100

    headers: init files

---
+++ incs/main.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 18:01:59 by bguyot            #+#    #+#             */
+/*   Updated: 2023/12/14 18:05:11 by bguyot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <t_42sh.h>
+
+/**
+ *	@brief	Print a error message and exit if needed
+ *	@param	error	Informations about the error
+ */
+void	error(t_error	error);
