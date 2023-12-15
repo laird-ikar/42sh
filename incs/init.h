2a4eaa52fc52362e865b50bde1d6e5d0e8e37018
laird-ikar <bguyot@student.42mulhouse.fr>
2023-12-15 11:18:10 +0100

    init: create functions init and tini

--- incs/init.h
+++ incs/init.h
@@ -6,8 +6,20 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:07:29 by bguyot            #+#    #+#             */
-/*   Updated: 2023/12/15 11:07:34 by bguyot           ###   ########.fr       */
+/*   Updated: 2023/12/15 11:16:27 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+/**
+ *	@brief	Allocate the data needed for 42sh
+ *	@param	data	A pointer to the data structure
+ */
+void	init(t_42sh *data);
+
+/**
+ *	@brief	Deallocate the data needed for 42sh
+ *	@param	data	A pointer to the data structure
+ */
+void	tini(t_42sh *data);
--- srcs/init.c
+++ srcs/init.c
@@ -6,8 +6,20 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:07:04 by bguyot            #+#    #+#             */
-/*   Updated: 2023/12/15 11:07:09 by bguyot           ###   ########.fr       */
+/*   Updated: 2023/12/15 11:14:36 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <init.h>
+#include <t_42sh.h>
+#include <errror.h>
+
+void	init(t_42sh *data)
+{
+	set_errror(data, OK, "", !FATAL);
+}
+
+void	tini(t_42sh *data)
+{
+	set_errror(data, NOT_IMPLEMENTED, "tini: Not Implemented", FATAL);
+}
